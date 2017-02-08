4d57fdb9b00ecdc2af6f01f22c862000b7945ba2
Michael Crilly <mmc@sent.com>
2017-02-08 18:51:36 +1000

    - Introducing some templates and files for Nginx and Gogs - Introducing Let's Encrypt to the equation

--- flibble.tf
+++ flibble.tf
@@ -1,4 +1,9 @@
 provider "digitalocean" {}
+
+data "template_file" "flibble_shell_script" {
+  template = "${file("${path.module}/flibble.sh")}"
+}
+
 resource "digitalocean_droplet" "flibble" {
   count = 1
   image = "centos-7-x64"
@@ -7,5 +12,5 @@ resource "digitalocean_droplet" "flibble" {
   size = "512mb"
   backups = true
   ssh_keys = ["1207665"]
-  user_data = "${file("${path.module}/flibble.sh")}"
+  user_data = "${data.template_file.flibble_shell_script.rendered}"
 }
