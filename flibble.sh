eb3b77c1d029baea70ac67db0b190fcecd1682eb
Michael Crilly <mmc@sent.com>
2017-02-08 21:21:16 +1000

    Come on DO - work it out...

--- flibble.sh
+++ flibble.sh
@@ -3,4 +3,4 @@
 yum update -y 
 yum install epel-release -y 
 yum install ansible git -y
-ansible-pull flibble.yaml -U https://github.com/mrcrilly/MrFlibble.git -f --clean --accept-host-key -i localhost, -e mrflibble_letsencrypt_email="${mrflibble_letsencrypt_email}" -e mrflibble_domain_name="${mrflibble_subdomain_name}.${mrflibble_domain_name}"
+ansible-pull flibble.yaml -U ${mrflibble_git_repository} -f --clean --accept-host-key -i localhost, -e mrflibble_letsencrypt_email="${mrflibble_letsencrypt_email}" -e mrflibble_domain_name="${mrflibble_subdomain_name}.${mrflibble_domain_name}"
